c947ca4b92867eac7ba3193cc0cd0b6e74e91d12
francomilanesioait <francomilanesioait@gmail.com>
2025-05-07 11:43:17 -0300

    ADD: Kill def is_temperature_greater_than_twenty_eight

--- services/geo_api_service.py
+++ services/geo_api_service.py
@@ -8,23 +8,6 @@ class GeoAPI():
     LON = LON
 
     @classmethod
-    def is_temperature_greater_than_twenty_eight(
-        self,
-        temperature: float
-    ) -> bool:
-        '''
-        Funcion encargada de verificar si la temperatura pasada por parametro es mayor
-        a 28°C
-
-        :param temperature `float`: Temperatura en grados Celcius.
-
-        :return False `bool`si la temperatura es menor:
-        :return True `bool`si la temperatura es mayor:
-        '''
-        return temperature > 28
-
-
-    @classmethod
     def is_hot_in_pehuajo(self):
         """
         Funcion encargada de consultar el clima en pehuajo y determinar
@@ -42,21 +25,15 @@ class GeoAPI():
             
             if req.status_code >= 400:
                 return {
-                'results': False,
-                'temperature': None
-            }
+                    'results': False,
+                    'temperature': None
+                }
             
             if req.status_code == 200:
                 data = req.json()
-
                 temperature = data['main']['temp']
-                
-                result = self.is_temperature_greater_than_twenty_eight(
-                    temperature=temperature
-                )
-                
                 return {
-                    'results': result,
+                    'results': temperature > 28,
                     'temperature': temperature
                 }
             
